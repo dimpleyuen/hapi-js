9a64547c79dec1d362cd5fe24ab9252f318e9923
dimpleyuen <dimpleyuen@gmail.com>
2015-04-13 06:35:10 +0000

    nitrous changes for hapi

---
+++ index.js
@@ -0,0 +1,22 @@
+var Hapi = require('hapi');
+var server = new Hapi.Server();
+
+//configure connection
+server.connection({
+  host: '0.0.0.0',
+  port: 8080,
+  routes: {cors: true} //means its cool if someone wants to send ajax request from outside
+});
+
+//extends hapi with a plugin, which will take care of the routes for us
+var plugins = [{ register: require('./routes/quotes.js') }]; 
+
+//load the server with the plugins
+server.register(plugins, function (err) {
+  if (err) { throw err; } //checks for errors, or else start the server
+
+//start the server
+  server.start(function () {
+    console.log('info', 'Server running at: ' + server.info.uri);
+  });
+});
